ca00ce31a70759038b8e815b03fcfa721677fca2
Rajith Vidanaarachchi <rajith.vidanaarachchi@gmail.com>
2016-03-02 10:31:16 +0530

    Ruby wrappers for one argument functions

--- ext/symengine/ruby_function.c
+++ ext/symengine/ruby_function.c
@@ -1,7 +1,7 @@
 #include "ruby_function.h"
 
 
-VALUE cfunction_func(void (*cwfunc_ptr)(basic_struct*, const basic_struct*), VALUE operand1) {
+VALUE cfunction_onearg(void (*cwfunc_ptr)(basic_struct*, const basic_struct*), VALUE operand1) {
     basic_struct *cresult;
     VALUE result;
 
@@ -17,19 +17,42 @@ VALUE cfunction_func(void (*cwfunc_ptr)(basic_struct*, const basic_struct*), VAL
     return result;
 }
 
-
-VALUE cfunction_abs(VALUE self, VALUE operand1) {  
-    return cfunction_func(basic_abs, operand1);
+#define IMPLEMENT_ONE_ARG_FUNC(func) \
+VALUE cfunction_ ## func(VALUE self, VALUE operand1) { \
+<<<<<<< HEAD
+    return cfunction_onearg(basic_ ## func, operand1); \
+=======
+    return cfunction_func(basic_ ## func, operand1); \
+>>>>>>> 9a4b4c7... Ruby wrappers for one argument functions
 }
 
-VALUE cfunction_sin(VALUE self, VALUE operand1) {  
-    return cfunction_func(basic_sin, operand1);
-}
+IMPLEMENT_ONE_ARG_FUNC(abs);
+IMPLEMENT_ONE_ARG_FUNC(sin);
+IMPLEMENT_ONE_ARG_FUNC(cos);
+IMPLEMENT_ONE_ARG_FUNC(tan);
+IMPLEMENT_ONE_ARG_FUNC(csc);
+IMPLEMENT_ONE_ARG_FUNC(sec);
+IMPLEMENT_ONE_ARG_FUNC(cot);
+IMPLEMENT_ONE_ARG_FUNC(asin);
+IMPLEMENT_ONE_ARG_FUNC(acos);
+IMPLEMENT_ONE_ARG_FUNC(asec);
+IMPLEMENT_ONE_ARG_FUNC(acsc);
+IMPLEMENT_ONE_ARG_FUNC(atan);
+IMPLEMENT_ONE_ARG_FUNC(acot);
+IMPLEMENT_ONE_ARG_FUNC(sinh);
+IMPLEMENT_ONE_ARG_FUNC(cosh);
+IMPLEMENT_ONE_ARG_FUNC(tanh);
+IMPLEMENT_ONE_ARG_FUNC(csch);
+IMPLEMENT_ONE_ARG_FUNC(sech);
+IMPLEMENT_ONE_ARG_FUNC(coth);
+IMPLEMENT_ONE_ARG_FUNC(asinh);
+IMPLEMENT_ONE_ARG_FUNC(acosh);
+IMPLEMENT_ONE_ARG_FUNC(asech);
+IMPLEMENT_ONE_ARG_FUNC(acsch);
+IMPLEMENT_ONE_ARG_FUNC(atanh);
+IMPLEMENT_ONE_ARG_FUNC(acoth);
+IMPLEMENT_ONE_ARG_FUNC(lambertw);
+IMPLEMENT_ONE_ARG_FUNC(dirichlet_eta);
+IMPLEMENT_ONE_ARG_FUNC(zeta);
+IMPLEMENT_ONE_ARG_FUNC(gamma);
 
-VALUE cfunction_cos(VALUE self, VALUE operand1) {  
-    return cfunction_func(basic_cos, operand1);
-}
-
-VALUE cfunction_tan(VALUE self, VALUE operand1) {  
-    return cfunction_func(basic_tan, operand1);
-}
