d913a157c69ddced09f84d56e98a71c15608c930
John Woods <john.woods@intuitivemachines.com>
2016-04-07 09:27:55 -0700

    Fixes #33. Adds SymEngine::symbols() method which produces multiple symbols simultaneously. Allows Symbol.new to take a Ruby symbol or a string.

--- ext/symengine/ruby_symbol.c
+++ ext/symengine/ruby_symbol.c
@@ -1,10 +1,20 @@
 #include "ruby_symbol.h"
 
-VALUE csymbol_init(VALUE self, VALUE name) {
-    Check_Type(name, T_STRING);
-    basic_struct *this;
-    char *str_ptr = StringValueCStr(name);
+VALUE csymbol_init(VALUE self, VALUE name_or_id) {
+    char *str_ptr;
 
+    switch (TYPE(name_or_id)) {
+    case T_STRING:
+      str_ptr = StringValueCStr(name_or_id);
+      break;
+    case T_SYMBOL:
+      str_ptr = rb_id2name(rb_to_id(name_or_id));
+      break;      
+    default:
+      rb_raise(rb_eTypeError, "wrong argument type %s (expected Symbol or String)", rb_obj_classname(name_or_id));
+    }
+
+    basic_struct *this;
     Data_Get_Struct(self, basic_struct, this);
 
     symbol_set(this, str_ptr);
