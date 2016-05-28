81ade4c2e038fc63cf09162be866a22468d64daf
rajithv <rajith.vidanaarachchi@gmail.com>
2016-05-28 15:26:24 +0530

    Complex Spec

--- spec/complex_spec.rb
+++ spec/complex_spec.rb
@@ -1,62 +1,51 @@
-require 'spec_helper'
+describe SymEngine::Complex do
+  context '#initialize' do
+    context 'with a Complex' do
+      subject { SymEngine(Complex(2, 3)) }
 
-describe SymEngine do
-  describe SymEngine::Complex do
-    describe '.new' do
-      context 'with a Ruby Complex object as input' do
-        it 'returns an instance of SymEngine::Complex class' do
-          a = Complex(2, 3)
-          a = SymEngine::convert(a)
-          expect(a).to be_an_instance_of SymEngine::Complex
-          expect(a.to_s).to eq('2 + 3*I')
-        end
-      end
-      context 'with a Ruby Integer as input' do
-        it 'returns an instance of SymEngine::Integer class' do
-          a = Complex(2, 0)
-          a = SymEngine::convert(a)
-          expect(a).to be_an_instance_of SymEngine::Integer
-          expect(a.to_s).to eq('2')
-        end
-      end
-      context 'Obtaining the integer real part' do
-        it 'returns an instance of SymEngine::Integer class' do
-          a = Complex(2, 7)
-          a = SymEngine::convert(a)
-          a = a.real
-          expect(a).to be_an_instance_of SymEngine::Integer
-          expect(a.to_s).to eq('2');
-        end
-      end
-      context 'Obtaining the integer imaginary part' do
-        it 'returns an instance of SymEngine::Integer class' do
-          a = Complex(2, 7)
-          a = SymEngine::convert(a)
-          a = a.imaginary
-          expect(a).to be_an_instance_of SymEngine::Integer
-          expect(a.to_s).to eq('7');
-        end
-      end
-      context 'Obtaining the rational real part ' do
-        it 'returns an instance of SymEngine::Rational class' do
-          a = Rational('5/4')
-          a = Complex(a, 7)
-          a = SymEngine::convert(a)
-          a = a.real
-          expect(a).to be_an_instance_of SymEngine::Rational
-          expect(a.to_s).to eq('5/4');
-        end
-      end
-      context 'Obtaining the rational imaginary part' do
-        it 'returns an instance of SymEngine::Integer class' do
-          a = Rational('5/4')
-          a = Complex(2, a)
-          a = SymEngine::convert(a)
-          a = a.imaginary
-          expect(a).to be_an_instance_of SymEngine::Rational
-          expect(a.to_s).to eq('5/4');
-        end
-      end
+      it { is_expected.to be_a SymEngine::Complex }
+      its(:to_s) { is_expected.to eq '2 + 3*I' }
+    end
+
+    context 'with an integer' do
+      subject { SymEngine(Complex(2, 0)) }
+
+      it { is_expected.to be_a SymEngine::Integer }
+      its(:to_s) { is_expected.to eq '2' }
     end
   end
+
+  context 'real_part and imaginary_part' do
+    let(:a) { SymEngine(Complex(Rational('2/7'), Rational('3/8'))) }
+    let(:b) { SymEngine(Complex(2, 3)) }
+
+    context 'real_part' do
+      context 'using SymEngine Rationals' do
+        subject { a.real }
+        it { is_expected.to be_a SymEngine::Rational }
+        its(:to_s) { is_expected.to eq '2/7' }
+      end
+
+      context 'using SymEngine Integers' do
+        subject { b.real }
+        it { is_expected.to be_a SymEngine::Integer }
+        its(:to_s) { is_expected.to eq '2' }
+      end
+    end
+
+    context 'imaginary_part' do
+      context 'using SymEngine Rationals' do
+        subject { a.imaginary }
+        it { is_expected.to be_a SymEngine::Rational }
+        its(:to_s) { is_expected.to eq '3/8' }
+      end
+
+      context 'using SymEngine Integers' do
+        subject { b.imaginary }
+        it { is_expected.to be_a SymEngine::Integer }
+        its(:to_s) { is_expected.to eq '3' }
+      end
+    end 
+  end
 end
+
