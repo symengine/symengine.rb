9cdab35151883c84e618c1f1e5e87343ed8a6c22
rajithv <rajith.vidanaarachchi@gmail.com>
2016-06-03 12:46:17 +0530

    Float, String constructors for RealMPFR working

--- ext/symengine/ruby_real_mpfr.c
+++ ext/symengine/ruby_real_mpfr.c
@@ -2,21 +2,26 @@
 
 VALUE crealmpfr_init(VALUE self, VALUE num_value, VALUE prec_value) {
     basic_struct *cresult;
-    VALUE result;
     double d;
+    char *c;
     int prec = NUM2INT(prec_value);
-    cresult = basic_new_heap();
+
+    Data_Get_Struct(self, basic_struct, cresult);
 
     switch( TYPE(num_value) ) {
         case T_FLOAT:
             d = RFLOAT_VALUE(num_value);     
             real_mpfr_set_d(cresult, d, prec);
             break;
+        case T_STRING:
+            c = RSTRING_PTR(num_value);
+            real_mpfr_set_str(cresult, c, prec);
+            break;
         default:
             rb_raise(rb_eTypeError, "Invalid Type: Float, BigDecimal or String required.");
             break;
     }
-    result = Data_Wrap_Struct(c_real_mpfr, NULL, cbasic_free_heap, cresult);
-    return result;
+
+    return self;
 }
 
