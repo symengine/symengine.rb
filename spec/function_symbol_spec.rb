310b7bf03b4e2d856150a088072f1e75ce29d537
Rajith Vidanaarachchi <rajith.vidanaarachchi@gmail.com>
2016-06-10 21:51:14 +0530

    Added specs for FunctionSymbol

---
+++ spec/function_symbol_spec.rb
@@ -0,0 +1,33 @@
+describe SymEngine::FunctionSymbol do
+
+  let(:x) { sym('x') }
+  let(:y) { sym('y') }
+  let(:z) { sym('z') }
+
+  describe '.new' do
+    context 'with symbols' do
+      subject { SymEngine::FunctionSymbol.new('f', x, y, z) }
+      it { is_expected.to be_a SymEngine::FunctionSymbol }
+    end
+
+    context 'with compound arguments' do
+      subject { SymEngine::FunctionSymbol.new('f', 2*x, y, SymEngine::sin(z)) }
+      it { is_expected.to be_a SymEngine::FunctionSymbol }
+    end
+  end
+  
+  context '#diff' do
+    let(:fun) { (SymEngine::FunctionSymbol.new('f', 2*x, y, SymEngine::sin(z))) }
+    context 'by variable' do
+        subject { fun.diff(x)/2 }
+        it { is_expected.to be_a SymEngine::Subs }
+      end
+
+      context 'by constant' do
+        subject { fun.diff(2) }
+        it { is_expected.to be_nil }
+      end
+  end
+end
+
+
