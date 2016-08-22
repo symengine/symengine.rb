7e9ca53ffd308422254f7144795d58372658b24e
Rajith Vidanaarachchi <rajith.vidanaarachchi@gmail.com>
2016-08-22 15:16:53 +0530

    Added more tests for parser

--- spec/symengine_spec.rb
+++ spec/symengine_spec.rb
@@ -29,5 +29,41 @@ describe SymEngine do
   it 'gives parse errors' do
     expect { SymEngine::parse('12a + n34a9') }.to raise_error(RuntimeError)
   end
+  
+  describe 'parse an Add' do
+    subject { SymEngine::parse('x + (y * 3 - 5)') }
+    
+    it { is_expected.to be_a SymEngine::Add }
+  end
+  
+  describe 'parse a Pow' do
+    subject { SymEngine::parse('x ** y') }
+    
+    it { is_expected.to be_a SymEngine::Pow }
+  end
+  
+  describe 'parse rational values' do
+    subject { SymEngine::parse('1 / 3 + 3 / 2') }
+    
+    it { is_expected.to eq SymEngine(11)/SymEngine(6) }
+  end
+  
+  describe 'parse integer expressions' do
+    subject { SymEngine::parse('1 + 2 * 2') }
+    
+    it { is_expected.to eq 5 }
+  end
+  
+  describe 'parse trig functions' do
+    subject { SymEngine::parse('sin(x) + cos(y) / 2') }
+    
+    it { is_expected.to be_a SymEngine::Add }
+  end
+  
+  describe 'parse functions' do
+    subject { SymEngine::parse('tan(0) - sqrt(2)') }
+    
+    it { is_expected.to be_a SymEngine::Mul }
+  end
 
 end
