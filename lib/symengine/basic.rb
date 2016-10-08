4864984b34b04e6047cf376333cb19d000a21535
Isuru Fernando <isuruf@gmail.com>
2016-10-08 15:57:14 +0530

    Better fix for Metrics/AbcSize
    Credits to @zverok

--- lib/symengine/basic.rb
+++ lib/symengine/basic.rb
@@ -15,22 +15,27 @@ module SymEngine
     end
 
     def to_proc(*args)
-      if args.empty?
-        if free_symbols.count > 1
-          raise ArgumentError, 'You should provide symbols order for #to_proc'\
-                               '. Only formulae with 1 free symbol can deduce'\
-                               ' its name automatically (#{free_symbols.count}'\
-                               ' found in #{self}).'
-        end
-        return SymEngine.lambdify(self, free_symbols.map { |s| s })
-      else
-        if free_symbols.count > args.length
-          raise ArgumentError, 'Formula contains #{free_symbols.count} free '\
-                               'symbols. You should provide at least this number '\
-                               'of arguments (only #{args.length} given).'
-        end
-        return SymEngine.lambdify(self, args)
+      SymEngine.lambdify(self, args.empty? ? default_args : check_args(*args))
+    end
+
+    def default_args
+      if free_symbols.count > 1
+        raise ArgumentError, 'You should provide symbols order for #to_proc'\
+                             '. Only formulae with 1 free symbol can deduce'\
+                             " its name automatically (#{free_symbols.count}"\
+                             " found in #{self})."
       end
+      free_symbols.to_a
+    end
+
+    def check_args(*args)
+      if free_symbols.count > args.length
+        raise ArgumentError, "Formula contains #{free_symbols.count} free "\
+                             'symbols. You should provide at least this number '\
+                             "of arguments (only #{args.length} given)."
+      end
+
+      args
     end
   end
 end
